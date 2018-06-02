7fb68b36c43f3592bf5a589819557d33bbdfccff
Alisson Chiquitto <chiquitto@gmail.com>
2018-06-01 20:59:41 -0400

    Cap 3 - Exercicio 2

---
+++ cap3/ex2.py
@@ -0,0 +1,28 @@
+'''
+Exercicio 2 - Pagina 38
+
+Os alunos de um determinado Instituto Federal realizaram uma prova de Física e obtiveram as seguintes notas:
+
+5, 8, 5, 6, 7, 5, 6, 8
+4, 7, 5, 5, 5, 8, 5, 6
+2, 5, 6, 0, 6, 5, 6, 7
+6, 6, 7, 2, 7, 6, 8, 3
+4, 9, 0, 1, 7, 6, 3, 4
+
+Construa, a partir dessas notas, os gráficos de:
+a. Dispersão
+b. Polígono de frequência
+c. Frequência acumulada
+d. Histograma
+e. Pictograma
+'''
+
+dados = [
+    5, 8, 5, 6, 7, 5, 6, 8,
+    4, 7, 5, 5, 5, 8, 5, 6,
+    2, 5, 6, 0, 6, 5, 6, 7,
+    6, 6, 7, 2, 7, 6, 8, 3,
+    4, 9, 0, 1, 7, 6, 3, 4
+]
+
+print(dados)
